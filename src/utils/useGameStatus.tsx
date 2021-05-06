2c0d29f8597ed5fc1c28875a15ff34ae7f8ba3e7
Shaun Gedye <sgedye88@yahoo.com.au>
2021-05-06 19:46:04 +0800

    Dodgy fix for rowsCleared

--- src/utils/useGameStatus.tsx
+++ src/utils/useGameStatus.tsx
@@ -1,22 +1,26 @@
 import { useState, useEffect, useCallback } from "react";
 
+const linePoints = [40, 100, 300, 1200];
+
 export const useGameStatus = (rowsCleared: number) => {
   const [score, setScore] = useState<number>(0);
   const [rows, setRows] = useState<number>(0);
   const [level, setLevel] = useState<number>(0);
 
   const calcScore = useCallback(() => {
-    const linePoints = [40, 100, 300, 1200];
-
     if (rowsCleared > 0) {
-      setScore((prev) => prev + linePoints[rowsCleared - 1] * (level + 1));
-      setRows((prev) => prev + rowsCleared);
+      // For some reason the rowsCleared is being doubled...
+      // this shouldn't happen, it could be to do with the double rendering...
+      // anyway, that is what we are dividing rows cleared by 2.
+      console.log(rowsCleared, linePoints[rowsCleared - 1]);
+      setScore((prev) => prev + linePoints[rowsCleared / 2 - 1] * (level + 1));
+      setRows((prev) => prev + rowsCleared / 2);
     }
   }, [level, rowsCleared]);
 
   useEffect(() => {
     calcScore();
-  }, [calcScore]);
+  }, [calcScore, rowsCleared, score]);
 
   return { score, setScore, rows, setRows, level, setLevel };
 };
